60ed1010291ee03c0fca4c89b7db168c46978f87
Saeed Ghezel Arsalan <saeedghezelarsalan@gmail.com>
2023-04-25 16:53:25 +0330

    add styles to avatar.style.tsx

--- src/lib/avatar/avatar.style.tsx
+++ src/lib/avatar/avatar.style.tsx
@@ -1,5 +1,29 @@
 export const SIZES = {
-  large: 'h-16 w-16',
-  medium: 'h-12 w-12',
+  tiny: 'h-6 w-6',
   small: 'h-8 w-8',
+  medium: 'h-10 w-10',
+  large: 'h-12 w-12',
+  huge: 'h-14 w-14',
+  whacking: 'h-16 w-16'
 };
+
+export const STATUS_SIZES = {
+  tiny: '8',
+  small: '12',
+  medium: '12',
+  large: '16',
+  huge: '16',
+  whacking: '16'
+};
+
+export const AVATAR_SHAPE = {
+  rectangle: 'rounded-xl',
+  rounded: 'rounded-full'
+}
+
+export const STATUS_POSITION = {
+  topRight:'top-0 right-0',
+  topLeft:'top-0 left-0',
+  bottomRight:'bottom-0 right-0',
+  bottomLeft:'bottom-0 left-0',
+}
