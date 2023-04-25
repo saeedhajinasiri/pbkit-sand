db365c1b4039d67e8906bc58092b2c25f7111f2a
Saeed Ghezel Arsalan <saeedghezelarsalan@gmail.com>
2023-04-25 16:55:12 +0330

    rebuild component and add shape , position to avatar

--- src/stories/avatar/avatar.stories.tsx
+++ src/stories/avatar/avatar.stories.tsx
@@ -23,6 +23,9 @@ export const Default = Template.bind({});
 const SizeTemplate: ComponentStory<typeof Avatar> = ({...args}: AvatarProps) => {
   return (
     <div className={'grid grid-cols-1 gap-10'}>
+      <Wrapper title={'Tiny'}>
+        <Avatar size={'tiny'} {...args}/>
+      </Wrapper>
       <Wrapper title={'Small'}>
         <Avatar size={'small'} {...args}/>
       </Wrapper>
@@ -32,7 +35,52 @@ const SizeTemplate: ComponentStory<typeof Avatar> = ({...args}: AvatarProps) => 
       <Wrapper title={'Large'}>
         <Avatar size={'large'} {...args}/>
       </Wrapper>
+      <Wrapper title={'Huge'}>
+        <Avatar size={'huge'} {...args}/>
+      </Wrapper>
+      <Wrapper title={'Whacking'}>
+        <Avatar size={'whacking'} {...args}/>
+      </Wrapper>
     </div>
   );
 }
 export const Size = SizeTemplate.bind({})
+
+
+
+const ShapeTemplate: ComponentStory<typeof Avatar> = ({...args}: AvatarProps) => {
+  return (
+    <div className={'grid grid-cols-1 gap-10'}>
+      <Wrapper title={'Rounded'}>
+        <Avatar shape={'rounded'} {...args}/>
+      </Wrapper>
+      <Wrapper title={'Rectangle'}>
+        <Avatar shape={'rectangle'} {...args}/>
+      </Wrapper>
+
+    </div>
+  );
+}
+export const Shape = ShapeTemplate.bind({})
+
+
+const AvatarPositionTemplate: ComponentStory<typeof Avatar> = ({...args}: AvatarProps) => {
+  return (
+    <div className={'grid grid-cols-1 gap-10'}>
+      <Wrapper title={'Top Right'}>
+        <Avatar position={'topRight'} {...args}/>
+      </Wrapper>
+      <Wrapper title={'Top Left'}>
+        <Avatar position={'topLeft'} {...args}/>
+      </Wrapper>
+      <Wrapper title={'Bottom Right'}>
+        <Avatar position={'bottomRight'} {...args}/>
+      </Wrapper>
+      <Wrapper title={'Bottom Left'}>
+        <Avatar position={'bottomLeft'} {...args}/>
+      </Wrapper>
+
+    </div>
+  );
+}
+export const AvatarPosition = AvatarPositionTemplate.bind({})
